b8ec558bc201a506b1207ce6453c8567ae1338ee
Molex <mateososa134@hotmail.com>
2023-11-10 04:00:05 -0300

    Parcial 2 modelo, DONE!

--- Parciales/src/parcial2/literatura/Libro.java
+++ Parciales/src/parcial2/literatura/Libro.java
@@ -6,13 +6,17 @@ import java.util.Objects;
  * Se recibe una lista de libros que por diversos problemas de carga, contiene
  * muchos libros repetidos. Implementar el método eliminarLibrosRepetidos que
  * devuelva otra lista de Libros, pero sin libros repetidos y ordenada por ISBN.
+ * 
+ * De cada Libro se registran el título, autor, año de edición y el ISBN (entero
+ * de 10 dígitos). El ISBN es un identificador único para libros, previsto para
+ * uso comercial.
  */
 
-public class Libro implements Comparable<Libro>{
+public class Libro implements Comparable<Libro> {
 
 	private Long isbn;
 	private String titulo;
-	
+
 	public Libro(Long isbn, String titulo) {
 		this.isbn = isbn;
 		this.titulo = titulo;
@@ -33,12 +37,12 @@ public class Libro implements Comparable<Libro>{
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
-	
+
 	@Override
 	public int compareTo(Libro p) {
 		return Long.compare(this.isbn, p.getIsbn());
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (obj == null || getClass() != obj.getClass())
@@ -48,10 +52,10 @@ public class Libro implements Comparable<Libro>{
 		Libro other = (Libro) obj;
 		return isbn == other.getIsbn() && Objects.equals(titulo, other.getTitulo());
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Tit: " + titulo + ", ISBN: " + isbn;
 	}
-	
+
 }
--- Parciales/src/parcial2/literatura/Literatura.java
+++ Parciales/src/parcial2/literatura/Literatura.java
@@ -6,7 +6,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -28,7 +28,7 @@ public class Literatura {
 		FileWriter fw = null;
 		BufferedReader br = null;
 		PrintWriter pw = null;
-		
+
 		try {
 			fr = new FileReader(archivoOrigen);
 			br = new BufferedReader(fr);
@@ -53,33 +53,68 @@ public class Literatura {
 			pw.close();
 		}
 	}
-	
-	public List<Libro> eliminarLibrosRepetidos(List<Libro> libros){
+
+	public List<Libro> eliminarLibrosRepetidos(List<Libro> libros) {
 		Set<Libro> aux = new TreeSet<Libro>(libros);
-		
+
 		List<Libro> listaSinLibrosRepetidos = new ArrayList<>(aux);
-		
+
 		return listaSinLibrosRepetidos;
 	}
-	
+
+	/**
+	 * Escribir una función sumar que reciba un arreglo de String y devuelva la suma
+	 * de sus elementos, para lo cual debe tratar de convertir cada elemento a un
+	 * número. En caso de no poder convertir algún elemento, debe ser ignorado,
+	 * capturando las excepciones que puedan llegar a ocurrir en el proceso y
+	 * mostrándolas, junto a sus mensajes, al finalizar el programa. Añadir una
+	 * cláusula finally, y dentro de ella imprimir un mensaje en pantalla para
+	 * probar que siempre se ejecuta esa parte del código.
+	 */
+
+	public void sumarElementosDeUnArray(List<String> elementos) {
+		int suma = 0;
+		List<String> problemas = new ArrayList<String>();
+		
+		for (String string : elementos) {
+			try {
+				Integer numero = Integer.parseInt(string);
+				suma += numero;
+			} catch (NumberFormatException e) {
+				problemas.add(e.getLocalizedMessage());
+			} finally {
+				System.out.println("finally");
+			}
+		}
+		System.out.println("La suma es: " + suma);
+		
+		System.out.println("\nProblemas que surgieron: ");
+		for (String string : problemas)
+			System.out.println(string);
+	}
+
 	public static void main(String[] args) {
 		Literatura lit = new Literatura();
-		
+
 		lit.reemplazar("Texto.txt", "TextoReemplazado.txt", "kit", "set");
-		
+
 		List<Libro> libros = new ArrayList<>();
-		
-		libros.add(new Libro((long)(2123), "Koala"));
-		libros.add(new Libro((long)(21), "Loki"));
-		libros.add(new Libro((long)(21), "Loki"));
-		libros.add(new Libro((long)(735), "Aveztrus"));
-		libros.add(new Libro((long)(2123), "Koala"));
-		
+
+		libros.add(new Libro((long) (2123), "Koala"));
+		libros.add(new Libro((long) (21), "Loki"));
+		libros.add(new Libro((long) (21), "Loki"));
+		libros.add(new Libro((long) (735), "Aveztrus"));
+		libros.add(new Libro((long) (2123), "Koala"));
+
 		List<Libro> nuevoslibros = lit.eliminarLibrosRepetidos(libros);
-		
+
 		for (Libro libro : nuevoslibros)
 			System.out.println(libro);
-		
+
+		List<String> elementos = new ArrayList<String>(Arrays.asList("Hola","13","a","4","a45","-12"));
+
+		lit.sumarElementosDeUnArray(elementos);
+
 	}
 
 }
