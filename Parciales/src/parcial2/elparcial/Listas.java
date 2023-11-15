ca7f1bad2e59b86ac5091b9727ed7f8476b2a700
Molex <mateososa134@hotmail.com>
2023-11-15 17:37:02 -0300

    Arreglado el punto 2.

--- Parciales/src/parcial2/elparcial/Listas.java
+++ Parciales/src/parcial2/elparcial/Listas.java
@@ -3,27 +3,28 @@ package parcial2.elparcial;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 2. Escribir un método que reciba dos listas como parámetros y devuelva true
+ * si todos los elementos de la primera son menores que cualquiera de la
+ * segunda, y false en caso contrario Considere que los objetos que están
+ * contenidos en las listas tienen órden total (Implementan Comparable y tienen
+ * implementado su propio equals y hashCode). Ejemplifica su uso mediante dos
+ * casos de prueba JUnit, uno con listas de Integer y otro con listas de Strings.
+ * 
+ *  public boolean sonTodosMenores(List<T> l1, List<T> l2) /**
+ *  
+ **/
+
 public class Listas<T extends Comparable<T>> {
 	public boolean todosSonMenores(List<T> l1, List<T> l2) {
 		Collections.sort(l1);
 		Collections.sort(l2);
-		
-		if (l1.get(l1.size() - 1).compareTo(l2.get(l2.size() - 1)) == -1)
-			return true;
-		else
-			return false;
-//		return l1.get(l1.size() - 1).compareTo(l2.get(l2.size() - 1)) < 0;
+
+		return l1.get(l1.size() - 1).compareTo(l2.get(0)) < 0;
 	}
-	/** ALTERNATIVA
-	 * public boolean todosSonMenores(List<T> l1, List<T> l2) {
-    for (T elem1 : l1) {
-        for (T elem2 : l2) {
-            if (elem1.compareTo(elem2) >= 0) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
+	/**
+	 * ALTERNATIVA public boolean todosSonMenores(List<T> l1, List<T> l2) { for (T
+	 * elem1 : l1) { for (T elem2 : l2) { if (elem1.compareTo(elem2) >= 0) { return
+	 * false; } } } return true; }
 	 */
 }
